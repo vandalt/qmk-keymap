55911273d320b5e0e94ca8a51d1d254b7a90d6d1
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-06-10 14:49:47 -0400

    Move caps word to left pinky

--- users/vandalt/vandalt.c
+++ users/vandalt/vandalt.c
@@ -15,8 +15,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQUAL,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,
     KC_BSPC,        MT(MOD_LSFT, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),MT(MOD_LGUI, KC_F),LT(2, KC_G),                                    LT(1, KC_H),    MT(MOD_RGUI, KC_J),MT(MOD_RCTL, KC_K),MT(MOD_LALT, KC_L),MT(MOD_RSFT, KC_SCLN),KC_QUOTE,
-    TG(4),          MT(MOD_LCTL, KC_Z),MT(MOD_LSFT, KC_X),KC_C,           MT(MOD_RALT, KC_V),LT(3, KC_B),                                    KC_N,           MT(MOD_RALT, KC_M),KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
-                                                    LT(1, KC_ESCAPE),KC_LEFT_GUI,                                    CW_TOGG,        LT(2, KC_SPACE)
+    CW_TOGG,          MT(MOD_LCTL, KC_Z),MT(MOD_LSFT, KC_X),KC_C,           MT(MOD_RALT, KC_V),LT(3, KC_B),                                    KC_N,           MT(MOD_RALT, KC_M),KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
+                                                    LT(1, KC_ESCAPE),KC_LEFT_GUI,                                    TG(4),        LT(2, KC_SPACE)
   ),
   [1] = LAYOUT_LR(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
