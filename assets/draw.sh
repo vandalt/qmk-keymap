2fc0e0b39a8288421fded6f3cad9b9ceb7fad1e6
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-12-24 16:30:13 -0500

    Add media layer to draw command

--- assets/draw.sh
+++ assets/draw.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-qmk c2json keyboards/zsa/voyager/keymaps/vandalt/keymap.c | keymap -c keymap_drawer.config.yaml parse --layer-names Base Sym Nav Mouse NoMod -c 10 -q - > assets/voyager_keymap.yaml
+qmk c2json keyboards/zsa/voyager/keymaps/vandalt/keymap.c | keymap -c ./assets/keymap_drawer.config.yaml parse --layer-names Base Sym Nav Media Mouse NoMod -c 10 -q - > assets/voyager_keymap.yaml
 keymap draw assets/voyager_keymap.yaml > ./assets/voyager_keymap.ortho.svg
